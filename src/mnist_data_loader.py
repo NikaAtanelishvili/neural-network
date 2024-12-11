c5c393e6b4abae9e718b766ab9abedeb0bca8adb
NikaAtanelishvili <atanelishvilinika@gmail.com>
2024-12-11 21:06:40 +0400

    feat: artificially increasing training examples by rotating images

--- src/mnist_data_loader.py
+++ src/mnist_data_loader.py
@@ -1,9 +1,22 @@
 import gzip
 import pickle
+import random
 
+from PIL import Image
 import numpy as np
+from matplotlib import pyplot as plt
+from numpy import dtype
 
-def load_mnist_data(file_path, chunk_size=5000):
+
+def rotate_image(image, max_angle=15):
+    # angle = random.uniform(max_angle, max_angle)
+    sfx = random.choice([-1, 1])
+    angle = max_angle * sfx
+    img = Image.fromarray(np.reshape(image, (28, 28)) * 255)
+    rotated_img =  img.rotate(angle, resample=Image.BILINEAR, expand=False)
+    return np.array(rotated_img).flatten() / 255.0
+
+def load_mnist_data(file_path, training_chunk_size=1000, test_chunk_size=5000):
     with gzip.open(file_path, 'rb') as f:
         tr_d, _, te_d  = pickle.load(f, encoding='latin1')  # Use 'latin1' for compatibility
 
@@ -12,9 +25,29 @@ def load_mnist_data(file_path, chunk_size=5000):
     training_data = np.array(list(zip(training_inputs, training_results)), dtype=object)
 
     test_inputs = [np.reshape(x, (784, 1)) for x in te_d[0]]
-    test_data = list(zip(test_inputs[:chunk_size], te_d[1]))
+    test_data = list(zip(test_inputs, te_d[1]))
 
-    return training_data, test_data
+    # Artifi
+    if training_chunk_size >= 50000:
+        # Rotate training images
+        rotated_images = []
+        rotated_labels = []
+        for (img, label) in zip(tr_d[0], tr_d[1]):
+            rotated_img = rotate_image(img)
+            rotated_images.append(np.reshape(rotated_img, (784, 1)))
+            rotated_labels.append(vectorized_result(label))
+
+            if len(rotated_images) > training_chunk_size:
+                break
+
+        rotated_training_data = np.array(list(zip(rotated_images, rotated_labels)), dtype=object)
+
+        augmented_training_data = np.concatenate((training_data, rotated_training_data), axis=0)
+
+        return augmented_training_data[:training_chunk_size], test_data[:test_chunk_size]
+
+    return training_data[:training_chunk_size], test_data[:test_chunk_size]
+
 
 def vectorized_result(j):
     e = np.zeros((10, 1))
